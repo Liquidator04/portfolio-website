0e964374b45a6a42f0a38de26af132a2148daf7d
Liquidator04 <aritrabag04@gmail.com>
2024-09-14 00:28:49 +0530

    Added Dark Mode

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/toggle-mode";
+
+export default function Home() {
+  return (
+    <>
+      <div className="m-4 flex gap-4">
+        <h1 className="flex justify-items-center items-center">Hello world</h1>
+        <div className="flex justify-items-center items-center">
+          <ModeToggle />
+        </div>
+      </div>
+
+      <Button className="ml-4">Click Here</Button>
+    </>
+  );
+}
