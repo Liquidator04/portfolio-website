87ed4f7f352bba7d1d4284acbab282ab75642589
Liquidator04 <aritrabag04@gmail.com>
2024-09-16 23:47:34 +0530

    Tested  aceternity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
-import Image from "next/image";
+import { SpotlightPreview } from "@/components/hero";
+import { FloatingNavDemo } from "@/components/navbar";
 import "./globals.css";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/toggle-mode";
-import { FloatingDockDemo } from "@/components/float-dock";
-import { BackgroundBeamsDemo } from "@/components/hero";
 
 export default function Home() {
   return (
     <>
-      <div className="m-4 flex gap-4">
-        <h1 className="flex justify-items-center items-center">Hello world</h1>
-        <div className="flex justify-items-center items-center">
-          <ModeToggle />
-        </div>
-      </div>
-
-      <Button className="ml-4">Click Here</Button>
-      <FloatingDockDemo />
-      <BackgroundBeamsDemo />
+      <FloatingNavDemo />
+      <SpotlightPreview />
     </>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,29 +1,25 @@
-"use client";
 import React from "react";
-import { BackgroundBeams } from "@/components/ui/background-beams";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "@/components/ui/spotlight";
 
-export function BackgroundBeamsDemo() {
+export function SpotlightPreview() {
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Join the waitlist
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> is the new trend.
         </h1>
-        <p></p>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-          Welcome to MailJet, the best transactional email service on the web.
-          We provide reliable, scalable, and customizable email solutions for
-          your business. Whether you&apos;re sending order confirmations,
-          password reset emails, or promotional campaigns, MailJet has got you
-          covered.
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
         </p>
-        <input
-          type="text"
-          placeholder="hi@manuarora.in"
-          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-        />
       </div>
-      <BackgroundBeams />
     </div>
   );
 }
